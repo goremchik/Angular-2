afdcf0ab22f87d21a37bdaf30ef5d69b6e7106b2
Andrii Shoferivskyi <Andrii_Shoferivskyi@epam.com>
2017-06-23 19:10:15 +0300

    finished 3d lesson about forms

---
+++ app/media-item-form.component.ts
@@ -0,0 +1,25 @@
+import { Component, Inject } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'mw-media-item-form',
+  templateUrl: 'app/media-item-form.component.html',
+  styleUrls: ['app/media-item-form.component.css']
+})
+
+export class MediaItemFormComponent {
+    form;
+
+    ngOnInit() {
+        this.form = new FormGroup({
+            medium: new FormControl('Movies'),
+            name: new FormControl(''),
+            category: new FormControl(''),
+            year: new FormControl(''),
+        });
+    }
+
+    onSubmit(mediaItem) {
+        console.log(mediaItem);
+    }
+}
